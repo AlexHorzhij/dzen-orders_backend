10b3f791fe89fb9f7573efcb77800b866e3599f8
AlexHorzhij <97761791+AlexHorzhij@users.noreply.github.com>
2024-09-22 21:54:16 +0300

    add dotenv

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
